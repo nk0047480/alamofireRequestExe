c1040d524c245c14df1588994fcf2f80e7377d97
JiaHung Hsia <x120565903@gmail.com>
2019-07-05 14:40:02 +0800

    Initial Commit

---
+++ alamofireRequestExe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  alamofireRequestExe
+//
+//  Created by Hsia JiaHung on 2019/7/5.
+//  Copyright © 2019年 MEMS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
