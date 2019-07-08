88089975a557c94fbf541843d82b162fcfeea54f
JiaHung Hsia <x120565903@gmail.com>
2019-07-08 11:50:21 +0800

    alamofire上傳

--- alamofireRequestExe/ViewController.swift
+++ alamofireRequestExe/ViewController.swift
@@ -7,14 +7,88 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
-
+    
+    var str = ""
+    
+    @IBOutlet weak var print_textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        print_textView.backgroundColor = UIColor.lightGray
     }
+    
+    @IBAction func response_btn(_ sender: UIButton) {
+        request01()
+    }
+    
+    
+    @IBAction func GET_btn(_ sender: UIButton) {
+        request02()
+    }
+    
+    
+    func request01() {
+        let url_file = "http://192.168.0.117/mySQL_exe/includeEXE.php"
+        Alamofire.request(url_file)
+            .response { response in
+                
+                //print(response.request as Any)  // 請求對象
+                //print(response.response as Any) // 響應對象
+                //print(response.data as Any)     // 伺服器回傳數據
+                
+                if response.data != nil {//解閉包，如果有東西則執行下列程式
+                    if let utf8Text = String(data: response.data!, encoding: .utf8){
+                        //print("Data: \(utf8Text)")
+                        self.str = String(utf8Text)
+                        //print(self.str)
+                        self.print_textView.text = self.str
+                    }
+                }
+        }
+        
+    }
+    
+    func request02() {
 
+        Alamofire.request("http://192.168.0.117/mySQL_exe/getEXE.php", parameters: ["term":"bar"])
+            .response { response in
+                /*print("REQUEST: \(response.request)")// original URL request
+                print("URL_RESPONSE: \(response.response)")// URL response
+                print("DATA: \(response.data)")// server data
+                print("RESULT: \(response.result)")// result of response serialization*/
+                /*if response.result.isSuccess {
+                    if let JSON = response.result.value {
+                        print("JSON: \(JSON)")//具体如何解析json内容可看下方“响应处理”部分
+                    }
+                    
+                    if response.data != nil {//解閉包，如果有東西則執行下列程式
+                        if let utf8Text = String(data: response.data!, encoding: .ascii){
+                            print("Data: \(utf8Text)")
+                        }
+                    } else {
+                        print("error")
+                    }
+                }*/
+
+                
+                if response.data != nil {//解閉包，如果有東西則執行下列程式
+                    if let utf8Text = String(data: response.data!, encoding: .utf8){
+                        print("Data:------------- \(utf8Text)")
+                    }
+                } else {
+                    print("error")
+                }
+                
+                
+                
+        }
+        
+    }
+    
 
 }
 
